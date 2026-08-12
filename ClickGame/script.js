335f7d32d4b54094af69fbd5dd4e3063b4600e71
sahardchd-ai <sahard.chd@gmail.com>
2026-08-13 00:04:55 +0700

    update Clickergame2
    Co-authored-by: SaharadMk <saharad.ch@gmail.com>

--- ClickGame/script.js
+++ ClickGame/script.js
@@ -1,23 +1,38 @@
 const scoreCount = document.querySelector("#scoreCount");
 const btnWatering = document.querySelector("#btn-watering");
+// const  = document.querySelector("");
+const reset = document.querySelector("#btn-reset");
+const localScore = "localScore"
+const upgradeStore = document.querySelector("#upgradeStore");
+
 // console.log(yourCount);
-let count = 0;
+let count = Number(localStorage.getItem(localScore)) || 0;
+let plusCount = 1;
 
-scoreCount.textContent =`${Math.floor(count)}`;
+scoreCount.textContent = `${Math.floor(count)}`;
 
-
+//กดปุ่มแล้วscoreเพิ่ม
 btnWatering.addEventListener(`click`, () => {
-    count+=1;
-    scoreCount.textContent = `${Math.floor(count)}`;
-    btnWatering.classList.remove(`treeActive`);
-    void btnWatering.offsetWidth;
-    btnWatering.classList.add(`treeActive`);
-    });
+  count += plusCount;
+  scoreCount.textContent = `${Math.floor(count)}`;
+  // 2. บันทึกค่า count ล่าสุดลง localStorage ทันทีที่คลิก
+  localStorage.setItem(localScore, count);
+
+  // เพิ่มอนิเมชันการกดปุ่ม
+  btnWatering.classList.remove(`treeActive`);
+  void btnWatering.offsetWidth;
+  btnWatering.classList.add(`treeActive`);
+});
+
+//เอาไว้รีscore
+reset.addEventListener(`click`, () => {
+  localStorage.removeItem(localScore); // ลบค่าออกจากความจำ
+  count = 0;
+  scoreCount.textContent = `${Math.floor(count)}`;
+});
 
 
-// const intervalId = setInterval(() => {
-//   count+=0.1;
-//   Math.floor(count);
-//   console.log(`Count: ${Math.floor(count)}`);
-//   yourCount.textContent = `${Math.floor(count)}`;
-// }, 100);//1000ms=1s
+// const  = document.querySelector("");
+// while(count = 15){
+//
+// };
